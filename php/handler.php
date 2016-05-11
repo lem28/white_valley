153b851c53c4a6668bf106895ac68ef94eab718e
Luis Montenegro <lem28@njit.edu>
2016-05-11 15:45:38 -0400

    Update 9: Garbage in, garbage out

--- php/handler.php
+++ php/handler.php
@@ -2,71 +2,92 @@
 require_once("inc/user.php.inc");
 require_once("inc/file.php.inc");
 
-$request = $_REQUEST["case"];
-$response = "<h1>didn't work :^)";
+$r = $_REQUEST['r'];
 
-switch($request)
+function register()
 {
-	case "register":
-		$json = json_decode(file_get_contents("php://input"));
-		$username = $json['reg_username'];
-		$password = $json['reg_password'];
-		$first_name = $json['reg_first_name'];
-		$last_name = $json['reg_last_name'];
-		$email = $json['reg_email'];
-		$login = new user("inc/connect.ini");
-		$response = $login->login_user($username, $password);
-		if (!$response['success'])
-		{
-			$login->add_new_user($username,$password,$first_name,$last_name,$email);
-			$response = "<p> $username Registered Successfully!";
-		}
-		else
-		{
-			$response = "<p>Registration Failed: ".$response['message'];
-		}
-		break;
-	case "login":
-		$json = json_decode(file_get_contents("php://input"));
-		$username = $json['log_username'];
-		$password = $json['log_password'];
-		$login = new user("inc/connect.ini");
-		$response = $login->login_user($username, $password);
-		if ($response['success'])
-		{
-			$response = "<p>Login Successful!";
-			$_SESSION["user"] = $username;
-			header( 'refresh:3; Location: http://127.0.0.1/user.html' ) ;
-		}
-		else
-		{
-			$response = "<p>Login Failed: ".$response['message'];
-		}
-		break;
-	case "logout":
-		session_unset();
-		session_destroy();
-		header( 'refresh:3; Location: http://127.0.0.1/index.html' ) ;
-		break;
-	case "upload":
-		$json = json_decode(file_get_contents("php://input"));
-		$file = new file("inc/connect.ini");
-		$response = $file->file_upload();
-		if ($response['success'])
-		{
-			$response = "<p>File Upload Successful!";
-			header( 'refresh:3; Location: http://127.0.0.1/user.html' ) ;
-		}
-		else
-		{
-			$response = "<p>File Upload Failed...";
-		}
-		break;
-	case "browse":
-		$json = json_decode(file_get_contents("php://input"));
-		$file = new file("inc/connect.ini");
-		$response = $file->file_browse();
-		break;
+	$json = json_decode(file_get_contents("php://input"), true);
+	$username = $json['reg_username'];
+	$password = $json['reg_password'];
+	$first_name = $json['reg_first_name'];
+	$last_name = $json['reg_last_name'];
+	$email = $json['reg_email'];
+	$login = new user("inc/connect.ini");
+	$response = $login->login_user($username, $password);
+	if (!$response['success'])
+	{
+		$login->add_new_user($username,$password,$first_name,$last_name,$email);
+		$response = "<p> $username Registered Successfully!";
+		echo $response;
+	}
+	else
+	{
+		$response = "<p>Registration Failed: ".$response['message'];
+		echo $response;
+	}
 }
-echo $response;
+
+function login()
+{
+	$json = json_decode(file_get_contents("php://input"), true);
+	$username = $json['log_username'];
+	$password = $json['log_password'];
+	$login = new user("inc/connect.ini");
+	$response = $login->login_user($username, $password);
+	if (!$response['success'])
+	{
+		$response = "<p>Login Successful!";
+		echo $response;
+		$_SESSION["user"] = $username;
+		header('Location: ../user.html');
+	}
+	else
+	{
+		$response = "<p>Login Failed: ".$response['message'];
+		echo $response;
+	}
+}
+
+function logout()
+{
+	session_unset();
+	session_destroy();
+	header('Location: ../index.html');
+}
+
+function upload()
+{
+	$json = json_decode(file_get_contents("php://input"), true);
+	$file = new file("inc/connect.ini");
+	$response = $file->file_upload();
+	if ($response['success'])
+	{
+		$response = "<p>File Upload Successful!";
+		echo $response;
+		header('../user.html');
+	}
+	else
+	{
+		$response = "<p>File Upload Failed...";
+		echo $response;
+	}
+}
+
+function browse()
+{
+	$file = new file("inc/connect.ini");
+	$response = $file->file_browse();
+}
+
+if($r == "register")
+	register();
+if($r == "login")
+	login();
+if($r == "logout")
+	logout();
+if($r == "upload")
+	upload();
+if($r == "browse")
+	browse();
+
 ?>
